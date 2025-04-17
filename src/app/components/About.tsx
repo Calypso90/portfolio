695b3622fd3ecc3a73cf80c99782b04bfe0d8c8e
Calypso <calypsotheatre@gmail.com>
2025-04-17 00:11:54 -0400

    Updated about section

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -1,27 +1,45 @@
 export default function About() {
   return (
     <section id="about" className="py-20">
-      <div className="container mx-auto px-6">
-        <h2 className="text-3xl font-bold text-center mb-8">About Me</h2>
-        <div className="max-w-3xl mx-auto text-center">
-          <p className="text-lg mb-4">
+      <div className="container mx-auto px-6 w-full">
+        <h2 className="text-3xl text-white font-bold text-center mb-8 bg-gradient-to-r from-blue-500 to-purple-600 py-2">
+          About Me
+        </h2>
+        <div className="max-w-3/4 mx-auto text-center">
+          <p className="text-lg mb-4 px-10">
             I'm a Software Developer with a solid foundation in front-end
             development and user experience design. I'm proficient in
             JavaScript, React, Next.js, and Tailwind CSS, and I draw on my
             background in theatrical design to create engaging, user-focused
-            applications that are both visually appealing and functional.
-          </p>
-          <p className="text-lg mb-4">
-            I focus on optimizing performance, ensuring cross-browser
+            applications that are both visually appealing and functional. I
+            focus on optimizing performance, ensuring cross-browser
             compatibility, and delivering seamless user experiences.
-            Collaboration is key for me—I enjoy working with cross-functional
+            Collaboration is key for me. I enjoy working with cross-functional
             teams and translating complex technical concepts into clear,
-            understandable language for non-technical users.
+            understandable language for non-technical users. Committed to
+            continuous learning, I’m always looking for ways to craft impactful
+            solutions that meet user needs and business goals. Let’s create
+            something amazing together!
           </p>
-          <p className="text-lg">
-            Committed to continuous learning, I’m always looking for ways to
-            craft impactful solutions that meet user needs and business goals.
-            Let’s create something amazing together!
+          <h2 className="text-3xl text-white font-bold text-center mt-10 mb-2 bg-gradient-to-r from-blue-500 to-purple-600 py-2">
+            Beyond the Code
+          </h2>
+          <p className="text-lg px-10">
+            With a professional background that bridges the worlds of theater,
+            education, and technology, I bring a creative, human-centered
+            approach to every project. My time in theatrical design taught me
+            the importance of storytelling, aesthetics, and teamwork, skills
+            that translate into my work in tech. Equally passionate about
+            education and community, I’ve had the privilege of speaking at
+            conferences like Orlando Code Camp and Atlanta Cloud Conference,
+            where I share insights on feedback, design, and community-building.
+            I believe knowledge is meant to be shared and that we all rise
+            together when we help others grow. I’m an active volunteer and
+            co-organizer in the Atlanta tech scene, where I contribute to
+            communities like Atlanta JavaScript, Modern Web, RenderATL, and
+            Torc. Whether I’m organizing events, mentoring newcomers, or helping
+            to build inclusive spaces, I’m committed to empowering others and
+            making tech more accessible and welcoming to all.
           </p>
         </div>
       </div>
--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -2,10 +2,10 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <section className="bg-gradient-to-r from-blue-500 to-purple-600 text-white py-20">
-      <div className="container px-6">
+    <section className="bg-gradient-to-r from-blue-500 to-purple-600 text-white py-10">
+      <div className="container px-8">
         <div className="flex flex-col md:flex-row items-center justify-center">
-          <div className="mb-8 md:mb-0 md:mr-8">
+          <div className="mb-6 md:mb-0 md:mr-8">
             <Image
               src="/headshot.png"
               alt="Calypso Hernandez"
@@ -15,13 +15,11 @@ export default function Hero() {
             />
           </div>
           <div className="text-center md:text-left">
-            <h1 className="text-4xl md:text-5xl font-bold mb-4">
+            <h1 className="text-3xl md:text-4xl font-bold mb-4">
               Calypso Hernandez
             </h1>
-            <p className="text-xl md:text-2xl mb-8">Software Developer</p>
             <p className="text-lg md:text-xl max-w-xl">
-              Blending creativity with code to build exceptional user
-              experiences.
+              Software Developer. Community Builder. Lifelong Learner.
             </p>
           </div>
         </div>
